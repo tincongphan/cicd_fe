0cb7ba25302a1c3815eeaa7af7afb1bde4ee25cd
congtin <tincongphan@gmail.com>
2023-09-07 13:37:10 +0700

    update content 2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,9 @@ import { NavLink } from 'react-router-dom';
 const Navbar = () => {
     return (
         <div className="topnav">
-            <NavLink to="/">hello world</NavLink>
-            <NavLink to="/contact">hello world vn</NavLink>
-            <NavLink to="/about">About ai 124</NavLink>
+            <NavLink to="/">hello world singapore</NavLink>
+            <NavLink to="/contact">hello world usa</NavLink>
+            <NavLink to="/about">About ai </NavLink>
         </div>
 
     )
